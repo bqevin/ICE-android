8520dda08cab09b0f0adda3c127edb5ab673ff2c
Kevin Barasa <kevin.barasa001@gmail.com>
2017-01-09 02:20:48 +0300

    Fixed AddICEActivity intent to avoid confusing main activity

--- app/src/main/java/com/projects/kevinbarassa/emergencyresponder/AddICEActivity.java
+++ app/src/main/java/com/projects/kevinbarassa/emergencyresponder/AddICEActivity.java
@@ -1,11 +1,11 @@
 package com.projects.kevinbarassa.emergencyresponder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class AddICEActivity extends AppCompatActivity {
 
@@ -16,15 +16,17 @@ public class AddICEActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton done_ice = (com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.done_button);
+        done_ice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Done adding ICE", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
--- app/src/main/java/com/projects/kevinbarassa/emergencyresponder/MainActivity.java
+++ app/src/main/java/com/projects/kevinbarassa/emergencyresponder/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,AddICEActivity.class);
                 startActivity(intent);
-                finish();
             }
 
         });
